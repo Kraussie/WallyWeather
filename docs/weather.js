346a0c9532c2b7f9daf07c0353b0020635eb1456
Kraussie <nathanialkrauss@gmail.com>
2020-05-21 10:50:05 -0400

    reset js

--- docs/weather.js
+++ docs/weather.js
@@ -1,3 +1,2 @@
-$.getJSON("prediction.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+var json = $.getJSON("prediction.json");
+console.log(json);
